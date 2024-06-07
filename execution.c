fbb1033f258995a602d2fc005fd82498690b7411
ABDE-LKADER <abadouab@student.1337.ma>
2024-06-07 15:22:42 +0100

    CHECK POINT <Fix Heap Over Fllow> ...

--- execution.c
+++ execution.c
@@ -6,7 +6,7 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:54:33 by abadouab          #+#    #+#             */
-/*   Updated: 2024/06/07 12:32:45 by abadouab         ###   ########.fr       */
+/*   Updated: 2024/06/07 15:22:29 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,22 @@ void	execution(t_minishell *ms, t_tree *tree, char **env)
 
 	if (!tree)
 		return ;
-	if (*(tree->value) != '|' && *(tree->value) != '&')
-	{
-		pid = fork();
-		if (pid == -1)
-			return ;
-		if (pid == 0)
-		{
-			path = "/bin/";
-			path = ft_strjoin(&ms->leaks, path, tree->value);
-			printf("VALUE: %s\t\t\tADDRESS: %p\n", tree->value, &tree);
-			printf("------------------ %d |", execve(path, tree->args, env));
-			exit(EXIT_SUCCESS);
-		}
-	}
+	(void)path;
+	(void)pid;
+	// if (*(tree->value) != '|' && *(tree->value) != '&')
+	// {
+	// 	pid = fork();
+	// 	if (pid == -1)
+	// 		return ;
+	// 	if (pid == 0)
+	// 	{
+	// 		path = "/bin/";
+	// 		path = ft_strjoin(&ms->leaks, path, tree->value);
+	// 		printf("VALUE: %s\t\t\tADDRESS: %p\n", tree->value, &tree);
+	// 		printf("------------------ %d |", execve(path, tree->args, env));
+	// 		exit(EXIT_SUCCESS);
+	// 	}
+	// }
 	execution(ms, tree->left, env);
 	execution(ms, tree->right, env);
 }
