d8f9caeaa1c4439f0a4deccb949c5590d32f7470
ABDE-LKADER <abadouab@student.1337.ma>
2024-06-08 02:43:58 +0100

    CHECK POINT <Execution Of The Commans Almost Done> ...

--- execution.c
+++ execution.c
@@ -6,34 +6,62 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:54:33 by abadouab          #+#    #+#             */
-/*   Updated: 2024/06/08 01:19:20 by abadouab         ###   ########.fr       */
+/*   Updated: 2024/06/08 02:43:26 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	execution(t_minishell *ms, t_tree *tree, char **env)
+char	*fetch_path(t_minishell *ms, t_environ *env, char *cmd)
+{
+	char	**paths;
+
+	while (env && ft_strncmp("PATH", env->var, ft_strlen("PATH")))
+		env = env->next;
+	paths = ft_split(&ms->leaks, env->val, ':');
+	while (*paths)
+	{
+		*paths = ft_strjoin(&ms->alloc, *paths, "/");
+		*paths = ft_strjoin(&ms->alloc, *paths, cmd);
+		printf("PATH: %s\n", *paths);
+		if (!access(*paths, X_OK))
+			return (*paths);
+		paths++;
+	}
+	return (NULL);
+}
+
+void	command_execute(t_minishell *ms, t_tree *tree, char **env)
 {
 	pid_t	pid;
 	char	*path;
 
+	pid = fork();
+	if (pid == -1)
+		return ;
+	if (pid == 0)
+	{
+		path = fetch_path(ms, ms->env, tree->value);
+		if (!path)
+			return ;
+		if (execve(path, tree->args, env) == -1)
+			(syntax_err(ms, "command not found\n", 127),
+				exit(EXIT_FAILURE));
+		exit(EXIT_SUCCESS);
+	}
+	wait(NULL);
+}
+
+void	execution(t_minishell *ms, t_tree *tree, char **env)
+{
+
 	if (!tree)
 		return ;
-	if (*(tree->value) != '|' && *(tree->value) != '&')
-	{
-		pid = fork();
-		if (pid == -1)
-			return ;
-		if (pid == 0)
-		{
-			path = "/bin/";
-			path = ft_strjoin(&ms->leaks, path, tree->value);
-			printf("VALUE: %s\t\t\tADDRESS: %p\n", tree->value, &tree);
-			printf("------------------ %d |", execve(path, tree->args, env));
-			exit(EXIT_SUCCESS);
-		}
-		wait(NULL);
-	}
+	printf("THE PLACE YOU ARE : %s\n", tree->value);
 	execution(ms, tree->left, env);
-	execution(ms, tree->right, env);
+	if (*(tree->value) != '|' && *(tree->value) != '&')
+		command_execute(ms, tree, env);
+	printf("THE PLACE YOU ARE : %s\n", tree->value);
+	if (!ft_strncmp(tree->value, "&&", ft_strlen("&&")))
+		execution(ms, tree->right, env);
 }
