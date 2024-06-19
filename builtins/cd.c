63538f374c0569955578c6a3fc1f67ddf80eda9c
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-06-19 01:16:09 +0100

    CHECK POIONT <Add unset + env builtins, LeakSanitizer: detected memory leaks> ...

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,18 +1,5 @@
 #include "minishell.h"
 
-void change_directory(const char* path) {
-    if (chdir(path) == -1) {
-        perror("chdir");
-    } else {
-        char cwd[1024];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            printf("Changed directory to: %s\n", cwd);
-        } else {
-            perror("getcwd");
-        }
-    }
-}
-
 void	ft_cd(t_minishell *ms, char **args)
 {
 	int			len;
