38aeeeebd8b28d1d1e84b581ec9a6f8689c509f0
Abdelkader Adouab <abadouab@e3r1p9.1337.ma>
2024-07-23 10:02:02 +0100

    CHECK POINT <Clean Files Comments + Unwanted Names> ...

--- execution/pipeline.c
+++ execution/pipeline.c
@@ -3,56 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   pipeline.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abbaraka <abbaraka@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 16:30:49 by abadouab          #+#    #+#             */
-/*   Updated: 2024/07/21 17:08:33 by abbaraka         ###   ########.fr       */
+/*   Updated: 2024/07/23 09:58:17 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// void	first_child_exec(t_minishell *ms, t_tree *tree, int fds[2])
-// {
-// 	dup2(fds[1], STDOUT_FILENO);
-// 	close(fds[1]);
-// 	close(fds[0]);
-// 	execution(ms, tree);
-// 	exit(ms->exit_status);
-// }
-
-// void	second_child_exec(t_minishell *ms, t_tree *tree, int fds[2])
-// {
-// 	dup2(fds[0], STDIN_FILENO);
-// 	close(fds[1]);
-// 	close(fds[0]);
-// 	execution(ms, tree);
-// 	exit(ms->exit_status);
-// }
-
-// void	pipeline_handler(t_minishell *ms, t_tree *tree)
-// {
-// 	int		fds[2];
-// 	pid_t	f_pid;
-// 	pid_t	s_pid;
-// 	int		status;
-
-// 	if (pipe(fds) == -1)
-// 		(perror("pipe"), cleanup_handler(ms));
-// 	f_pid = fork();
-// 	if (f_pid == -1)
-// 		(perror("fork"), cleanup_handler(ms));
-// 	if (f_pid == 0)
-// 		first_child_exec(ms, tree->left, fds);
-// 	s_pid = fork();
-// 	if (s_pid == -1)
-// 		(perror("fork"), cleanup_handler(ms));
-// 	if (s_pid == 0)
-// 		second_child_exec(ms, tree->right, fds);
-// 	(TRUE) && (close(fds[1]), close(fds[0]));
-// 	(TRUE) && (waitpid(s_pid, &status, 0), waitpid(f_pid, NULL, 0));
-// 	(WIFEXITED(status)) && (ms->exit_status = WEXITSTATUS(status));
-// }
 
 void	execute_child(t_minishell *ms, t_tree *tree)
 {
