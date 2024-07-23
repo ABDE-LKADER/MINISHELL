77ae4d8b7825bae0fe074c93b2f7ab2eab730a9c
Abdelkader Adouab <abadouab@e3r1p9.1337.ma>
2024-07-23 11:45:01 +0100

    CHECK POINT <Pipline Working Already + DONE> ...

--- execution/pipeline.c
+++ execution/pipeline.c
@@ -6,74 +6,13 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 16:30:49 by abadouab          #+#    #+#             */
-/*   Updated: 2024/07/23 11:14:07 by abadouab         ###   ########.fr       */
+/*   Updated: 2024/07/23 11:42:01 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 void	execute_child(t_minishell *ms, t_tree *tree)
-{
-	char	*path;
-
-	if (tree->type != CMD_T)
-		execution(ms, tree);
-	else
-	{
-		tree->no_print = 1;
-		expanding(ms, tree);
-		if (redirection(ms, tree) == -1)
-			exit(EXIT_FAILURE);
-		if (check_if_builtins(tree->value))
-			(built_in_execute(ms, tree), exit(ms->exit_status));
-		else
-		{
-			if (!tree->value || !*tree->value)
-				exit(EXIT_SUCCESS);
-			path = fetch_path(ms, ms->env, tree->value);
-			if (execve(path, tree->args, change_linked_to_double(ms)) == -1)
-				execution_errors(ms, tree, path);
-		}
-	}
-}
-
-int	find_redir_type(t_tree *tmp, t_redirection type)
-{
-	int	i;
-
-	i = 0;
-	while (i < tmp->redir_index)
-	{
-		if (tmp->redir[i].redirection == type)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	create_process(t_minishell *ms, t_tree *tree)
-{
-	pid_t	pid;
-	int		pipefd[2];
-
-	if (pipe(pipefd) == -1)
-		exit(EXIT_FAILURE);
-	pid = fork();
-	if (pid == -1)
-		(perror("fork"), kill(0, SIGINT), g_catch_signals = 2);
-	else if (pid == 0)
-	{
-		if (tree->next != NULL)
-			dup2(pipefd[1], STDOUT_FILENO);
-		(close(pipefd[1]), close(pipefd[0]));
-		execute_child(ms, tree);
-	}
-	else
-		(dup2(pipefd[0], STDIN_FILENO), close(pipefd[1]), close(pipefd[0]));
-	return (FALSE);
-}
-
-void	execute_last(t_minishell *ms, t_tree *tree)
 {
 	char	*path;
 
@@ -98,7 +37,28 @@ void	execute_last(t_minishell *ms, t_tree *tree)
 	}
 }
 
-void	last_command(t_minishell *ms, t_tree *tree, int *std)
+void	create_process(t_minishell *ms, t_tree *tree)
+{
+	pid_t	pid;
+	int		pipefd[2];
+
+	if (pipe(pipefd) == -1)
+		exit(EXIT_FAILURE);
+	pid = fork();
+	if (pid == -1)
+		(perror("fork"), kill(0, SIGINT), g_catch_signals = 2);
+	else if (pid == 0)
+	{
+		if (tree->next != NULL)
+			dup2(pipefd[1], STDOUT_FILENO);
+		(close(pipefd[1]), close(pipefd[0]));
+		execute_child(ms, tree);
+	}
+	else
+		(dup2(pipefd[0], STDIN_FILENO), close(pipefd[1]), close(pipefd[0]));
+}
+
+void	last_command(t_minishell *ms, t_tree *tree, t_fds fds)
 {
 	int		pid;
 	int		status;
@@ -107,13 +67,10 @@ void	last_command(t_minishell *ms, t_tree *tree, int *std)
 	if (pid == -1)
 		(perror("fork"));
 	if (pid == 0)
-		execute_last(ms, tree);
+		execute_child(ms, tree);
 	else
 	{
-		dup2(std[0], STDIN_FILENO);
-		dup2(std[1], STDOUT_FILENO);
-		close(std[0]);
-		close(std[1]);
+		restore_fds(fds);
 		waitpid(pid, &status, 0);
 		while (wait(NULL) != -1)
 			;
@@ -129,11 +86,9 @@ void	last_command(t_minishell *ms, t_tree *tree, int *std)
 void	pipeline_handler(t_minishell *ms, t_tree *tree)
 {
 	t_tree	*tmp;
-	int		std[2];
-	int		pid;
+	t_fds	fds;
 
-	std[0] = dup(STDIN_FILENO);
-	std[1] = dup(STDOUT_FILENO);
+	fds = save_fds();
 	tmp = tree->next;
 	while (tmp->next)
 	{
@@ -142,8 +97,8 @@ void	pipeline_handler(t_minishell *ms, t_tree *tree)
 			ms->exit_status = 1;
 			break ;
 		}
-		pid = create_process(ms, tmp);
+		create_process(ms, tmp);
 		tmp = tmp->next;
 	}
-	last_command(ms, tmp, std);
+	last_command(ms, tmp, fds);
 }
