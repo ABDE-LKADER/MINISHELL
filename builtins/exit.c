0f86d0504f11c86fb6fa012cdb221fe45c9bf812
Abdelhakim Baraka <abbaraka@e3r10p15.1337.ma>
2024-07-24 14:31:28 +0100

    ft_exit fixed and adding whitespaces in split_op

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,40 +6,67 @@
 /*   By: abbaraka <abbaraka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:55:24 by ||||||||          #+#    #+#             */
-/*   Updated: 2024/07/21 16:49:05 by abbaraka         ###   ########.fr       */
+/*   Updated: 2024/07/24 14:24:30 by abbaraka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	check_numerical(t_minishell *ms, char **args, int len)
+char	*check_more_args_in_one(t_minishell *ms, char **args)
 {
-	if (!((args[1][len] >= '0' && args[1][len] <= '9')
-	|| ((args[1][len] == '-' || args[1][len] == '+') && len == 0))
-	|| (ft_strlen(args[1]) == 19
-		&& ft_strncmp(args[1], "9223372036854775807", ft_strlen(args[1])) > 0)
-	|| (ft_strlen(args[1]) == 20
-		&& ft_strncmp(args[1], "-9223372036854775808", ft_strlen(args[1])) > 0))
-		(syntax_err(ms, args[1], "numeric argument required", 255),
-			exit(255));
+	char	**split;
+	int		len;
+
+	split = split_args(&ms->leaks, args[1], " \t\n\r\f\v");
+	len = 0;
+	while (split[len])
+		len++;
+	if (len > 1)
+		return (NULL);
+	return (split[0]);
+}
+
+static void	check_numerical(t_minishell *ms, char **args)
+{
+	char	*str;
+	int	i;
+
+	str = check_more_args_in_one(ms, args);
+	i = 0;
+	if (!str)
+		(syntax_err(ms, str, "numeric argument required", 255),
+				exit(255));
+	while (str[i])
+	{
+		if (!((str[i] >= '0' && str[i] <= '9')
+		|| ((str[i] == '-' || str[i] == '+') && i == 0))
+		|| (ft_strlen(str) == 19
+			&& ft_strncmp(str, "9223372036854775807", ft_strlen(str)) > 0)
+		|| (ft_strlen(str) == 20
+			&& ft_strncmp(str, "-9223372036854775808", ft_strlen(str)) > 0))
+			(syntax_err(ms, str, "numeric argument required", 255),
+				exit(255));
+		i++;
+	}
 }
 
 void	ft_exit(t_minishell *ms, t_tree *tree, char **args)
 {
 	int	len;
-	int	i;
 	int	exit_status;
 
-	(1) && (len = 0, i = 0);
+	(1) && (len = 0);
 	while (args[len])
 		len++;
 	(tree->no_print == 0) && (ft_putstr_fd("exit\n", 2), exit_status = 0);
 	(1) && (exit_status = 0);
-	while (len > 1 && args[1][i])
-	{
-		check_numerical(ms, args, i);
-		i++;
-	}
+	// while (len > 1 && args[1][i])
+	// {
+	// 	check_numerical(ms, args, i);
+	// 	i++;
+	// }
+	if (len > 1)
+		check_numerical(ms, args);
 	if (len > 2)
 		return (syntax_err(ms, "exit", "too many arguments", 1));
 	else if (len == 2)
